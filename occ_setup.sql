9dbdeb90e9faa5aae95490964a7e12061b01ea96
Gianina Paula Gonzalez <gprgonzalez@firstgen.com.ph>
2026-03-23 12:01:01 +0800

    update occ_setup.sql
    change FRLC_OCC.zip to FRLC_OCC_[DATE].zip and FGHPC_OCC.zip FGHPC_OCC_[DATE].zip

--- occ_setup.sql
+++ occ_setup.sql
@@ -45,37 +45,49 @@ VALUES
     'RESOURCE CONSTRAINTS-HYDRO', 
     'Water Level and Inflow Constraint. The nomination of the Casecnan Hydroelectric Power Plant is based on the availability of water for electricity generation. 
     
-    Seasonal Deration. Maximum allowable generation for Casecnan units is affected by net head elevation of Casecnan. This net head is derived from the difference between Casecnan Head pond elevation and its Tail race elevation (equivalent to Pantabangan reservoir elevation). Refer to attached CHEPP Plant Capability with Seasonal Deration for reference.', 
-    'Actual Dispatch Report. 
-CHEPP Plant Capability with Seasonal Deration.'),
+    Seasonal Deration. Maximum allowable generation for Casecnan units is affected by net head elevation of Casecnan. This net head is derived from the difference between Casecnan Head pond elevation and its Tail race elevation (equivalent to Pantabangan reservoir elevation). Refer to attached CHEPP Plant Capability with Seasonal Deration for reference.', 'FRLC_OCC_[DATE].zip'),
 (   'FGHPC','01PNTBNG_U01,01PNTBNG_U02','OCC',10,'OFFERED_CAPACITY = REGISTERED_CAPACITY','Usual','OTHER CAUSES','Erroneous Flag. The unit was complying with OCC rule and was offering its registered capacity of 60 MW','NONE'),
 
-(   'FGHPC','01PNTBNG_U01,01PNTBNG_U02','OCC',20,'OFFERED_CAPACITY = 0 AND IDR = 0 AND HAS_OUTAGE = 0','Usual','RESOURCE CONSTRAINTS-HYDRO','Water Level and Irrigation Diversion Requirement Constraint. Please refer to items C.1, C.2, C.3 of the attached affidavit. No generation on PHEPP due to inability of MHEPP to generate brought by zero IDR and water discharge. Please refer to the attached DOR for IDR, and Pantabangan & Masiway dam elevation.','Notice of IDR
- FGHPC DOR'),
+(   'FGHPC','01PNTBNG_U01,01PNTBNG_U02','OCC',20,'OFFERED_CAPACITY = 0 AND IDR = 0 AND HAS_OUTAGE = 0','Usual','RESOURCE CONSTRAINTS-HYDRO','Water Level and Irrigation Diversion Requirement Constraint. Please refer to items C.1, C.2, C.3 of the attached affidavit. No generation on PHEPP due to inability of MHEPP to generate brought by zero IDR and water discharge. Please refer to the attached DOR for IDR, and Pantabangan & Masiway dam elevation.','FGHPC_OCC_[DATE].zip'),
 
 (   'FGHPC','01PNTBNG_U01,01PNTBNG_U02','OCC',30,'OFFERED_CAPACITY = 0 AND IDR < 10 AND HAS_OUTAGE = 0','Usual','RESOURCE CONSTRAINTS-HYDRO','Water Level and Irrigation Diversion Requirement Constraint. Please refer to items C.1, C.2, C.3 of the submitted affidavit.
  
- No generation on PHEPP due to inability of MHEPP to generate brought by very low IDR and water discharge. Please refer to the attached DOR for IDR, and Pantabangan & Masiway dam elevation.','Notice of IDR
- FGHPC DOR
- Daily Gen'),
+ No generation on PHEPP due to inability of MHEPP to generate brought by very low IDR and water discharge. Please refer to the attached DOR for IDR, and Pantabangan & Masiway dam elevation.','FGHPC_OCC_[DATE].zip'),
 (   'FGHPC','01PNTBNG_U01,01PNTBNG_U02','OCC',40,'OFFERED_CAPACITY >= 0 AND IDR >= 10 AND HAS_OUTAGE = 0','Usual','RESOURCE CONSTRAINTS-HYDRO','For intervals with AS AGC Operation: To prevent possible failure acquired when lower commands are issued from the high limit as basepoint, the unit to be scheduled as AGC shall have a maximum limit of 1MW lower than the expected capability due to the current water elevation. Please refer to the attached FGHPC AGC Test Result. Under page 6, Item 2 of Operations Recommendation from NGCP.
 
 Water Level and Irrigation Diversion Requirement Constraint. Please refer to items C.1, C.2, C.3  of the submitted affidavit.
 
 For intervals with zero offers, this was due to the projected Masiway elevation at the end of the trading day. PHEPP units must always operate within the prescribed safety limits of Masiway. In compliance with the Irrigation Diversion Requirement (IDR), and to prevent Masiway Dam from exceeding its maximum elevation, Pantabangan generation was deliberately limited and strategically allocated to intervals with higher electricity demand. Supporting details on Masiway Dam elevation are provided in the attached DOR and Daily Gen file.
 
-Section 3.1.  PHEPP cannot operate at 120MW if the level of the water at the Pantabangan reservoir is below two hundred seven (207) meters above sea level.','Notice of IDR
- FGHPC DOR
- PHEPP AGC Test Report'),
+Section 3.1.  PHEPP cannot operate at 120MW if the level of the water at the Pantabangan reservoir is below two hundred seven (207) meters above sea level.','FGHPC_OCC_[DATE].zip'),
 (   'FGHPC','01PNTBNG_U01,01PNTBNG_U02','OCC',50,'OFFERED_CAPACITY = 0 AND IDR >= 10 AND HAS_OUTAGE = 1','Usual','OUTAGE','NA','NA'),
 (   'FGHPC','01MASIWA_G01','OCC',10,'OFFERED_CAPACITY = REGISTERED_CAPACITY','Usual','OTHER CAUSES','Erroneous Flag. The unit was complying with OCC rule and was offering its registered capacity of 12 MW','NONE'),
 (   'FGHPC','01MASIWA_G01','OCC',20,'OFFERED_CAPACITY = 0 AND IDR = 0 AND HAS_OUTAGE = 0','Usual','RESOURCE CONSTRAINTS-HYDRO','Water Level and Irrigation Diversion Requirement Constraint. Please refer to items C.1, C.2, C.3 of the submitted affidavit.
  
- Zero Irrigation Diversion Requirement (IDR) and water discharge. The dispatch of MHEPP is highly dependent on the IDR and water discharge, water flow through the turbines and the water level at the Masiway reservoir. Please refer to the attached DOR for the IDR and Masiway dam elevation.','Notice of IDR
- FGHPC DOR'),
-(   'FGHPC','01MASIWA_G01','OCC',30,'OFFERED_CAPACITY > 0 AND IDR > 0 AND HAS_OUTAGE = 0','Usual','RESOURCE CONSTRAINTS-HYDRO','Water Level and Irrigation Diversion Requirement Constraint. Please refer to items C.1, C.2, C.3 of the submitted affidavit. The dispatch of MHEPP is highly dependent on the IDR and water discharge, water flow through the turbines and the water level at the Masiway reservoir. Please refer to the attached DOR for the IDR and Masiway dam elevation.','Notice of IDR
- FGHPC DOR'),
+ Zero Irrigation Diversion Requirement (IDR) and water discharge. The dispatch of MHEPP is highly dependent on the IDR and water discharge, water flow through the turbines and the water level at the Masiway reservoir. Please refer to the attached DOR for the IDR and Masiway dam elevation.','FGHPC_OCC_[DATE].zip'),
+(   'FGHPC','01MASIWA_G01','OCC',30,'OFFERED_CAPACITY > 0 AND IDR > 0 AND HAS_OUTAGE = 0','Usual','RESOURCE CONSTRAINTS-HYDRO','Water Level and Irrigation Diversion Requirement Constraint. Please refer to items C.1, C.2, C.3 of the submitted affidavit. The dispatch of MHEPP is highly dependent on the IDR and water discharge, water flow through the turbines and the water level at the Masiway reservoir. Please refer to the attached DOR for the IDR and Masiway dam elevation.','FGHPC_OCC_[DATE].zip'),
 (   'FGHPC','01MASIWA_G01','OCC',40,'OFFERED_CAPACITY = 0 AND IDR >= 0 AND HAS_OUTAGE = 1','Usual','OUTAGE','NA','NA');
+
+CREATE OR REPLACE TABLE OCC_DATA (
+    ID INTEGER,
+    RESOURCE_ID VARCHAR(255),
+    CLASSIFY VARCHAR(255), -- CLASSIFICATION COLUMN
+    TRADING_HOUR TIMESTAMP_NTZ,
+    PROBABLE_BREACH_FLAG INTEGER,
+    VIOLATION_TYPE INTEGER,
+    REASON VARCHAR(255),
+    DATE_FROM TIMESTAMP_NTZ,
+    DATE_TO TIMESTAMP_NTZ,
+    BRIEF_DESCRIPTION VARCHAR(1000),
+    ATTACHMENTS VARCHAR(500), -- FILE COLUMN
+    STATUS VARCHAR(255),
+    REGISTERED_CAPACITY FLOAT,
+    OFFERED_CAPACITY FLOAT,
+    TOTAL_OFFERED_CAPACITY FLOAT,
+    SEASONAL_MAX_CAPABILITY INTEGER,
+    HAS_OUTAGE INTEGER
+);
+
 
 
 
@@ -94,11 +106,18 @@ Section 3.1.  PHEPP cannot operate at 120MW if the level of the water at the Pan
 
 
 SELECT * FROM OCC_CTRL_REF;
-
 SELECT * FROM OCC_CTRL_REF
 LIMIT 2;
 
+SELECT * FROM OCC_DATA;
+SELECT * FROM OCC_DATA
+LIMIT 2;
+
 TRUNCATE TABLE OCC_CTRL_REF;
+TRUNCATE TABLE OCC_DATA;
+
+INSERT INTO OCC_DATA
+VALUES(10, 'CASECN_TEST', 'Scheduled', '2026-01-25 11:00', 1, 10, null, null, null, null, null, 'For Submission', 82.5, 60, null, null, null);
 
 INSERT INTO OCC_CTRL_REF
 VALUES('FRLC', 
@@ -110,3 +129,8 @@ VALUES('FRLC',
     'TESTING', 
     'Currently testing control table', 
     'NONE');
+
+SELECT * FROM OCC_DATA
+WHERE ID = 10;
+
+
